f0a67423ba73733100280615ab86ad9e5a56c887
Matt Burgess <matt@myclient.info>
2022-04-24 14:35:30 +0700

    Extensive rewrites with Tags enum

--- app/Models/index.ts
+++ app/Models/index.ts
@@ -33,3 +33,18 @@ export enum LocationTypes {
   MapsLink,
   Address,
 }
+
+export enum Tags {
+  Literature = '0cc875a9-7338-495a-94b1-5aa28bf2bbd9',
+  Film = 'eae15870-b4b2-440f-b36e-78ef517e8bbe',
+  Sports = '604abd8b-11e0-419a-ab91-ab8abf430244',
+  Music = '607b3a19-04e8-4e35-b2c5-07cedd919695',
+  Science = 'c948e09c-2606-4eb3-9461-8334b3f71135',
+  Geography = '213467cd-10a0-4d9b-9f50-26374dd62963',
+  Nature = 'bb1d66ce-c7cf-43d4-a672-0d1006264ba0',
+  Gaming = 'f14334ea-7e8f-4dd6-b3ba-e4970f4c16bc',
+  Easy = '454df36f-34a0-4e75-ad28-e614873fae93',
+  Difficult = 'ce3bab2f-fec9-4b43-9fc1-53fcd0041f21',
+  Television = '9a46261d-db91-4c2e-8f65-3aefff8aeef1',
+  History = '786f4936-8378-4bcd-a18f-8a78b9637dfb',
+}
--- database/seeders/02-Question.ts
+++ database/seeders/02-Question.ts
@@ -1,6 +1,6 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
 import Env from '@ioc:Adonis/Core/Env';
-import { Question, User, QuestionTypes } from 'App/Models';
+import { Question, User, QuestionTypes, Tags } from 'App/Models';
 
 export default class QuestionSeeder extends BaseSeeder {
   public async run() {
@@ -12,8 +12,8 @@ export default class QuestionSeeder extends BaseSeeder {
       {
         text: 'In Doctor Who what does T.A.R.D.I.S. stand for?',
         userId,
-        answers: [{ text: 'Time And Relative Dimensions In Space', points: 1, sort: 0 }],
-        tags: ['9a46261d-db91-4c2e-8f65-3aefff8aeef1'],
+        answers: [{ text: 'Time And Relative Dimensions In Space' }],
+        tags: [Tags.Television],
       },
       {
         userId,
@@ -28,7 +28,7 @@ export default class QuestionSeeder extends BaseSeeder {
         userId,
         text: "In The Lord of The Rings, what was Gollum's original name",
         answers: [{ text: 'Smeagol' }],
-        tags: ['0cc875a9-7338-495a-94b1-5aa28bf2bbd9', 'eae15870-b4b2-440f-b36e-78ef517e8bbe'],
+        tags: [Tags.Literature, Tags.Film],
       },
       {
         userId,
@@ -40,7 +40,7 @@ export default class QuestionSeeder extends BaseSeeder {
           { text: 'Number Five', points: 0, sort: 3 },
           { text: 'Patient Zero', points: 0, sort: 4 },
         ],
-        tags: ['f14334ea-7e8f-4dd6-b3ba-e4970f4c16bc'],
+        tags: [Tags.Television],
       },
       {
         userId,
@@ -52,7 +52,7 @@ export default class QuestionSeeder extends BaseSeeder {
           { text: '2044', points: 0, sort: 3 },
           { text: '2077', points: 1, sort: 4 },
         ],
-        tags: ['f14334ea-7e8f-4dd6-b3ba-e4970f4c16bc'],
+        tags: [Tags.Gaming],
       },
       {
         userId,
@@ -64,9 +64,10 @@ export default class QuestionSeeder extends BaseSeeder {
           { text: '1906', points: 0, sort: 3 },
           { text: '1987', points: 0, sort: 4 },
         ],
+        tags: [Tags.History],
       },
-      { userId, text: 'What is the capital of Thailand?', answers: [{ text: 'Bangkok', points: 1 }] },
-      { userId, text: 'What is the most abundant metal in the human body?', answers: [{ text: 'Calcium', points: 1 }] },
+      { userId, text: 'What is the capital of Thailand?', answers: [{ text: 'Bangkok' }], tags: [Tags.Geography] },
+      { userId, text: 'What is the most abundant metal in the human body?', answers: [{ text: 'Calcium' }], tags: [Tags.Science] },
       {
         userId,
         text: 'One point for every actor who played a Doctor Who - canonical BBC TV series only',
@@ -86,12 +87,13 @@ export default class QuestionSeeder extends BaseSeeder {
           { text: 'Peter Capaldi', sort: 12 },
           { text: 'Jodie Whittaker', sort: 13 },
         ],
+        tags: [Tags.Television],
       },
       {
         userId,
         text: 'Coffee chain Starbucks takes its name from a character in which classic novel?',
         answers: [{ text: 'Moby Dick' }],
-        tags: ['0cc875a9-7338-495a-94b1-5aa28bf2bbd9'],
+        tags: [Tags.Literature],
       },
       {
         userId,
@@ -106,13 +108,13 @@ export default class QuestionSeeder extends BaseSeeder {
           { text: 'Agility', sort: 6 },
           { text: 'Luck', sort: 7 },
         ],
-        tags: ['f14334ea-7e8f-4dd6-b3ba-e4970f4c16bc'],
+        tags: [Tags.Gaming],
       },
       {
         userId,
         text: 'Also known as the "Demon Barber of Fleet Street" this English musical character\'s victims were then baked into pies.',
         answers: [{ text: 'Sweeney Todd' }],
-        tags: ['f14334ea-7e8f-4dd6-b3ba-e4970f4c16bc'],
+        tags: [Tags.Literature],
       },
       {
         userId,
@@ -129,49 +131,55 @@ export default class QuestionSeeder extends BaseSeeder {
         userId,
         text: 'An orrery is a model (often mechanical) of what?',
         answers: [{ text: 'The Solar System' }],
-        tags: ['c948e09c-2606-4eb3-9461-8334b3f71135'],
+        tags: [Tags.Science],
       },
       {
         userId,
         text: 'Named for a Beatles song, she was the first discovered fossil of Australopithecus afarensis',
         answers: [{ text: 'Lucy' }],
-        tags: ['c948e09c-2606-4eb3-9461-8334b3f71135'],
+        tags: [Tags.Science],
       },
       {
         userId,
         text: 'In the year did the Chernobyl Disaster occur?',
         answers: [{ text: '1986' }],
-        tags: ['786f4936-8378-4bcd-a18f-8a78b9637dfb'],
+        tags: [Tags.History],
       },
       {
         userId,
         text: 'Who was the first woman to win a Nobel Prize?',
         answers: [{ text: 'Marie Curie' }],
-        tags: ['c948e09c-2606-4eb3-9461-8334b3f71135'],
+        tags: [Tags.Science, Tags.History],
       },
       {
         userId,
         text: 'Who wrote the Sherlock Holmes novels?',
         answers: [{ text: 'Sir Arthur Conan Doyle' }],
-        tags: ['0cc875a9-7338-495a-94b1-5aa28bf2bbd9'],
+        tags: [Tags.Literature],
       },
       {
         userId,
         text: 'Who was the first president of the United States of America?',
         answers: [{ text: 'George Washington' }],
-        tags: ['454df36f-34a0-4e75-ad28-e614873fae93'],
+        tags: [Tags.History, Tags.Easy],
       },
       {
         userId,
         text: 'Name one of the two mammals that lays eggs.',
         answers: [{ text: 'Echidna' }, { text: 'Platypus' }],
-        tags: ['bb1d66ce-c7cf-43d4-a672-0d1006264ba0'],
+        tags: [Tags.Science],
       },
       {
         userId,
         text: 'What planet lies closest to the sun?',
-        answers: [{ text: 'Echidna' }],
-        tags: ['bb1d66ce-c7cf-43d4-a672-0d1006264ba0', 'bb1d66ce-c7cf-43d4-a672-0d1006264ba0'],
+        answers: [{ text: 'Mercury' }],
+        tags: [Tags.Science, Tags.Easy],
+      },
+      {
+        userId,
+        text: 'The Lord of the Rings was written by JRR Tolkien. But what does JRR stand for?',
+        answers: [{ text: 'John Ronald Reuel' }],
+        tags: [Tags.Literature, Tags.Difficult],
       },
       {
         userId,
@@ -179,6 +187,75 @@ export default class QuestionSeeder extends BaseSeeder {
         text: 'True or False: The platypus is venomous.',
         answers: [{ text: 'True' }],
         tags: ['bb1d66ce-c7cf-43d4-a672-0d1006264ba0'],
+      },
+      {
+        userId,
+        text: 'A point for each name of the four replicants Blade Runner’s Deckard was originally sent to “retire”?',
+        answers: [{ text: 'Priss' }, { text: 'Leon' }, { text: 'Roy Batty' }, { text: 'Zhora' }],
+        tags: [Tags.Film],
+      },
+      {
+        userId,
+        text: 'Which STD is commonly referred to as The Clap?',
+        answers: [{ text: 'Gonorrhea' }],
+        tags: [Tags.Science, Tags.Nature],
+      },
+      {
+        userId,
+        text: 'What metal is the most electrically conductive?',
+        answers: [{ text: 'Silver' }],
+        tags: [Tags.Science, Tags.Difficult],
+      },
+      {
+        userId,
+        text: 'What is the longest a person can theoretically serve as US president?',
+        answers: [{ text: '10 Years' }],
+        tags: [Tags.History, Tags.Difficult],
+      },
+      {
+        userId,
+        text: 'How many universities are in the famed Ivy League?',
+        answers: [{ text: '8' }],
+      },
+      {
+        userId,
+        type: QuestionTypes.MultipleCorrect,
+        text: 'One point for each university you can name in the Ivy League',
+        answers: [
+          { text: 'Yale' },
+          { text: 'Dartmouth' },
+          { text: 'Cornell' },
+          { text: 'Brown' },
+          { text: 'Harvard' },
+          { text: 'Columbia' },
+          { text: 'Princeton' },
+          { text: 'Pennsylvania' },
+        ],
+        tags: [Tags.Difficult],
+      },
+      {
+        userId,
+        text: 'Which is the only planet named for a Greek instead of Roman God?',
+        answers: [{ text: 'Uranus' }],
+        tags: [Tags.Science],
+      },
+      {
+        userId,
+        text: 'What is the largest moon in the solar system?',
+        answers: [{ text: 'Ganymede' }],
+        tags: [Tags.Science],
+      },
+      {
+        userId,
+        text: 'The planets Uranus, Saturn, Jupiter and Neptune are known as __ giants?',
+        answers: [{ text: 'Gas' }],
+        tags: [Tags.Science, Tags.Easy],
+      },
+      {
+        userId,
+        text: 'Rounded to the nearest whole number, what percentage of the solar system’s mass is the sun?',
+        answers: [{ text: '100%' }],
+        tags: [Tags.Science],
       },
     ];
 
